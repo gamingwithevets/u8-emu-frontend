f4904e14aeaacde91052bf52ab28aacd4516469a
gamingwithevets <thenewgwe@gmail.com>
2024-02-26 23:10:37 +0700

    Commit

--- peripheral.c
+++ peripheral.c
@@ -27,3 +27,36 @@ void core_step(struct u8_core *core, bool real_hw, int hwid) {
 			stop_accept[0] = true;
 	}
 }
+
+void read_emu_kb(struct u8_core *core, int idx, int hwid, bool sample) {
+	uint8_t seg = 0;
+	uint16_t addr;
+
+	if (hwid == 0) addr = 0xe800 + idx;
+	else if (hwid == 4) return read_mem_data(core, 4, 0x8e00 + idx);
+	else if (hwid == 5) {
+		if (sample) {
+			switch (idx) {
+				case 0: return read_mem_data(core, 8, 0x8e07);
+				case 1: return read_mem_data(core, 8, 0x8e05);
+				case 2: return read_mem_data(core, 8, 0x8e08);
+			}
+		} else return read_mem_data(core, 8, 0x8e00 + idx);
+	}
+	return read_mem_data(core, 0, 0x8e00 + idx);
+}
+
+void write_emu_kb(struct u8_core *core, int idx, uint8_t val, int hwid, bool sample) {
+	if (hwid == 0) write_mem_data(core, 0, 0xe800 + idx, val);
+	else if (hwid == 4) write_mem_data(core, 4, 0x8e00 + idx, val);
+	else if (hwid == 5) {
+		if (sample) {
+			switch (idx) {
+				case 0: write_mem_data(core, 8, 0x8e07, val); break;
+				case 1: write_mem_data(core, 8, 0x8e05, val); break;
+				case 2: write_mem_data(core, 8, 0x8e08, val); break;
+			}
+		} else return write_mem_data(core, 8, 0x8e00 + idx);
+	}
+	write_mem_data(core, 0, 0x8e00 + idx, val);
+}
